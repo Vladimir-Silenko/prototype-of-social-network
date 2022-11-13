23c8fcb904be7c20616cf0944ab19ef14ecf8009
Vladimir Silenko <vvs220592@gmail.com>
2022-11-13 17:18:55 +0400

    installed and started using redux-form lib

--- src/components/dialogs/messages/SendMessage.jsx
+++ src/components/dialogs/messages/SendMessage.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import styles from './SendMessage.module.css'
 
-// const Update = (e) => {
-//     let text = e.target.value
-//     props.Update(text)
-// }
-const works = (values) => {
-    alert(values.newMessageText)
-}
 const SendMessage = (props) => {
     const Send = (values) => props.Send(values.newMessageText)
     return <div className={styles.sendMessage}>
@@ -20,7 +13,6 @@ const AddMessageForm = (props) => {
         <Field
             component='textarea'
             placeholder='Type...'
-            // onChange={Update}
             name="newMessageText" />
         <button >Send</button>
     </form>

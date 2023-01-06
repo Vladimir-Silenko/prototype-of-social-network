99e728998451b64d4e58b0194a596dd50d592c1c
Vladimir Silenko <vvs220592@gmail.com>
2023-01-06 16:23:38 +0300

    added catching of every error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 // import ProfileContainer from './components/profile/profileContainer';
 import News from './components/news/News';
@@ -14,7 +14,7 @@ import spinner from './photo/spinner.svg'
 import { useEffect, } from 'react';
 import { initialiseAPP } from './redux/app-reducer';
 import store from './redux/redux-store';
-import styled from 'styled-components';
+import { useLayoutEffect } from 'react';
 const Dialogs = React.lazy(() => import('./components/dialogs/Dialogs'))
 const ProfileContainer = React.lazy(() => import('./components/profile/profileContainer'))
 
@@ -22,22 +22,37 @@ const App = (props) => {
   const init = useSelector(state => state.app.initialise)
   const dispatch = useDispatch()
 
+  const AllUncaughtErrors = (promiseRejectionEvent) => {
+    // alert("some error occured")
+    console.log("some error occured")
+  }
+
   useEffect(() => {
     dispatch(initialiseAPP())
+    window.addEventListener("unhandeledRejection", AllUncaughtErrors())
   }, [])
+
+  useLayoutEffect(() => {
+    return () => {
+      window.removeEventListener("unhandeledRejection", AllUncaughtErrors())
+    }
+  }, [])
+
+
   if (init === false) {
     return <img src={spinner} />
   }
-  const Error = styled.div`
-  color:lightgray;
-  width:100%;
-  height:100vh;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  font-weight:bold;
-  font-size:50px;
-  `
+  const Error = {
+    color: 'lightgray',
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: '50px',
+  }
+
   return (
     <div className="app-wrapper">
       <Header />
@@ -45,7 +60,7 @@ const App = (props) => {
       <div className='app-wrapper-content'>
         <Suspense fallback={<img src={spinner} />}>
           <Routes>
-
+            <Route path="/" element={<Navigate to={`/profile/`} />} />
             <Route path="/profile/:userId" element={<ProfileContainer />} />
             <Route path='/dialogs' element={<Dialogs />} />
             <Route path='/news' element={<News />} />
@@ -53,7 +68,7 @@ const App = (props) => {
             <Route path='/settings' element={<Settings />} />
             <Route path='/users' element={<Users />}></Route>
             <Route path='/login' element={<LogInPage />}></Route>
-            <Route path='*' element={<Error>404 NOT FOUND</Error>}></Route>
+            <Route path='*' element={<div style={Error}>404 NOT FOUND</div>}></Route>
           </Routes>
         </Suspense>
       </div>
